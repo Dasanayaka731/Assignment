7669b18510b384559bda559ae9b2894841330ba4
Dasun Dasanayaka <dasanayakabandara81@gmail.com>
2023-09-02 18:29:27 +0530

    Second Commit

--- weatherapp/src/components/HomePage.js
+++ weatherapp/src/components/HomePage.js
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../style/HomePageStyle.css";
 import axios from "axios";
 
 export default function HomePage() {
-  const [data, setData] = useState({
-    celcius: 10,
-    name: "Colombo",
-    humidity: 10,
-    speed: 2,
-  });
-  const [name, setName] = useState("");
+  const [celcius, setCelcius] = useState(0);
+  const [name, setName] = useState("Colombo");
+  const [humidity, setHumidity] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [searchName, setSearchName] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Colombo&appid=102d463c1f601db0bb76fd7f835bc806&units=metric`;
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        console.log(res.data);
+        setCelcius(res.data.main.temp);
+        setName(res.data.name);
+        setHumidity(res.data.main.humidity);
+        setSpeed(res.data.wind.speed);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    // Fetch and update current time and date
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      setCurrentTime(now.toLocaleTimeString());
+      setCurrentDate(now.toLocaleDateString());
+    }, 1000); // Update every 1 second
+
+    return () => {
+      // Cleanup the interval on component unmount
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const handleClick = () => {
-    if (name !== "") {
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=102d463c1f601db0bb76fd7f835bc806&units=matric`;
+    if (searchName !== "") {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchName}&appid=102d463c1f601db0bb76fd7f835bc806&units=metric`;
       axios
         .get(apiUrl)
         .then((res) => {
           console.log(res.data);
-          setData({
-            ...data,
-            celcius: res.data.main.temp,
-            name: res.data.name,
-            humidity: res.data.main.humidity,
-            speed: res.data.wind.speed,
-          });
-          console.log(res);
+          setCelcius(res.data.main.temp);
+          setName(res.data.name);
+          setHumidity(res.data.main.humidity);
+          setSpeed(res.data.wind.speed);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.error(err);
+        });
     }
   };
 
@@ -35,22 +61,22 @@ export default function HomePage() {
     <div className="main-container">
       <div className="main-div">
         <div className="d1">
-          <p>13 c</p>
-          <br />
-          <p>Colombo</p>
+          <p>{celcius}°C</p>
+          <p>{name}</p>
         </div>
         <div className="d3">
           <input
             type="search"
             name=""
             id=""
-            onChange={(e) => setName(e.target.value)}
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
           />
           <button onClick={handleClick}>Search</button>
         </div>
         <div className="d2">
-          <p>4:02 PM</p>
-          <p>2023/09/02,Saturday</p>
+          <p>{currentTime}</p>
+          <p>{currentDate}</p>
         </div>
       </div>
     </div>
